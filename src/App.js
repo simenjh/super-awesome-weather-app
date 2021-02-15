e5fc30985d278a45743c7f670d593636be21b52b
Simen Janbu Høgseth <simenjh1@gmail.com>
2021-02-15 20:24:19 +0100

    Initial project skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const locationForecastEndpoint = "";
+const convertPlaceToCoordinates = place => {};
 
 function App() {
+  const [place, setPlace] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
+  fetchWeatherData()... 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SearchComponent /> */}
+      {/* <Location /> */}
+      <CurrentConditionsComponent place={place} currentConditions={weatherData.currentConditions} />
+      {/* <FutureForecast /> */}
     </div>
   );
 }
